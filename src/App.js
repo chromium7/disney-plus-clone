2e20b090dff0dbe1981b3c0d95709e0f8ee7cdef
Christofer Saputra <christofersaputra29@gmail.com>
2021-06-22 08:18:06 +0700

    Add login page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Details from "./components/Detail";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Login from "./components/Login";
 import React from "react";
 
 function App() {
@@ -13,10 +14,13 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/detail">
             <Details />
           </Route>
-          <Route path="/">
+          <Route path="/" exact>
             <Home />
           </Route>
         </Switch>
